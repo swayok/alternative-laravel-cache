f324403f6056c3bbc5a74692840f517f474d4d49
Aleksandr Filippov <swayalex.work@gmail.com>
2026-06-25 11:35:26 +0200

    Fixed possible error in ArrayCachePool related to non-existing hierarchical cache keys.

--- AlternativeLaravelCache/Vendors/Array/ArrayCachePool.php
+++ AlternativeLaravelCache/Vendors/Array/ArrayCachePool.php
@@ -255,7 +255,7 @@ class ArrayCachePool extends AbstractCachePool implements HierarchicalPoolInterf
                 $array !== null
                 && array_key_exists($key, $array)
             );
-            if (array_key_exists($key, $array)) {
+            if ($has) {
                 $array = $array[$key];
             }
         }
